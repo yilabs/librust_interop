12428a84fcad8ccdae8e0863923e0935b422f592
mw66 <mingwu@gmail.com>
2022-06-03 12:29:06 -0400

    refine var names to Rust library name.

--- rust/src/synced.rs
+++ rust/src/synced.rs
@@ -10,20 +10,17 @@ type QueueT = Queue<u64>;
 
 type HandleT = usize;
 
-type HashMapT = DashMap<u64, u64>;  // DashMap that stores u64 value from D side
-type HashMapsT = Vec<HashMapT>;
+type DashMapT = DashMap<u64, u64>;  // DashMap that stores u64 value from D side
+type DashMapsT = Vec<DashMapT>;
 
 type SegQueueT = SegQueue<u64>;
-
-type QueueT = SegQueueT;
-
-type QueuesT = Vec<QueueT>;
+type SegQueuesT = Vec<SegQueueT>;
 
 
 // How do I create a global, mutable singleton?
 // https://stackoverflow.com/a/27826181
-static HASHMAPS: Lazy<RwLock<HashMapsT>> = Lazy::new(|| RwLock::new(vec![]));
-static   QUEUES: Lazy<RwLock<QueuesT  >> = Lazy::new(|| RwLock::new(vec![]));
+static  DASHMAPS: Lazy<RwLock< DashMapsT>> = Lazy::new(|| RwLock::new(vec![]));
+static SEGQUEUES: Lazy<RwLock<SegQueuesT>> = Lazy::new(|| RwLock::new(vec![]));
 
 /*
 fn main() {
@@ -37,7 +34,7 @@ fn main() {
 
 
 // NOTE: use RwLock to protect `dashmap_new` to be thread-safe, since it will modify the underlying container Vec
-// so in the most called function dashmap_get, dashmap_insert, the HASHMAPS.get() no need to be sync-ed
+// so in the most called function dashmap_get, dashmap_insert, the DASHMAPS.get() no need to be sync-ed
 // othewise, it will be very slow
 // return a handle
 macro_rules! create_function { ($func_name:ident, $cell:ident, $ctype:ty) => {
@@ -53,8 +50,8 @@ pub unsafe extern "C" fn $func_name() -> HandleT {
 
 }; }
 
-create_function!( dashmap_new, HASHMAPS, HashMapT);
-create_function!(segqueue_new,   QUEUES,   QueueT);
+create_function!( dashmap_new,  DASHMAPS,  DashMapT);
+create_function!(segqueue_new, SEGQUEUES, SegQueueT);
 
 macro_rules! get_handle_obj { ($container:ident, $handle:ident, $obj:ident, $code:block) => {
   let read = $container.read().unwrap();  // NOTE: we only use the read lock on the container Vec!
@@ -64,7 +61,7 @@ macro_rules! get_handle_obj { ($container:ident, $handle:ident, $obj:ident, $cod
 
 #[no_mangle]
 pub unsafe extern "C" fn dashmap_get(handle:HandleT, key:u64) -> u64 {
-  get_handle_obj!(HASHMAPS, handle, obj,
+  get_handle_obj!(DASHMAPS, handle, obj,
     { return *(obj.get(&key).unwrap()); }
   );
 }
@@ -73,7 +70,7 @@ pub unsafe extern "C" fn dashmap_get(handle:HandleT, key:u64) -> u64 {
 // return the old val if there is any; NOTE: otherwise, will return 0 (TODO?)
 #[no_mangle]
 pub unsafe extern "C" fn dashmap_insert(handle:HandleT, key:u64, val:u64) -> u64 {
-  get_handle_obj!(HASHMAPS, handle, obj,
+  get_handle_obj!(DASHMAPS, handle, obj,
     { match obj.insert(key, val) {
         Some(old) => return old,
         None      => return 0,
@@ -84,7 +81,7 @@ pub unsafe extern "C" fn dashmap_insert(handle:HandleT, key:u64, val:u64) -> u64
 
 #[no_mangle]
 pub unsafe extern "C" fn dashmap_length(handle:HandleT) -> usize {
-  get_handle_obj!(HASHMAPS, handle, obj,
+  get_handle_obj!(DASHMAPS, handle, obj,
     { return obj.len(); }
   );
 }
@@ -92,7 +89,7 @@ pub unsafe extern "C" fn dashmap_length(handle:HandleT) -> usize {
 
 #[no_mangle]
 pub unsafe extern "C" fn segqueue_pop(handle:HandleT) -> u64 {
-  get_handle_obj!(QUEUES, handle, obj,
+  get_handle_obj!(SEGQUEUES, handle, obj,
     { return obj.pop().unwrap(); }
   );
 }
@@ -100,7 +97,7 @@ pub unsafe extern "C" fn segqueue_pop(handle:HandleT) -> u64 {
 // return bool: ok or err
 #[no_mangle]
 pub unsafe extern "C" fn segqueue_push(handle:HandleT, val:u64) -> bool {
-  get_handle_obj!(QUEUES, handle, obj,
+  get_handle_obj!(SEGQUEUES, handle, obj,
     { obj.push(val); return true; }
   );
 }
@@ -108,15 +105,15 @@ pub unsafe extern "C" fn segqueue_push(handle:HandleT, val:u64) -> bool {
 // NOTE: _destroy and _length are D's convention
 #[no_mangle]
 pub unsafe extern "C" fn segqueue_length(handle:HandleT) -> usize {
-  get_handle_obj!(QUEUES, handle, obj,
+  get_handle_obj!(SEGQUEUES, handle, obj,
     { return obj.len(); }
   );
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn segqueue_destroy(handle:HandleT) {
-  get_handle_obj!(QUEUES, handle, obj,
-    { drop(obj); }  // TODO: also remove from QUEUES vec?
+  get_handle_obj!(SEGQUEUES, handle, obj,
+    { drop(obj); }  // TODO: also remove from SEGQUEUES vec?
   );
 }
 
